adding tilt selection network fix rotation/flip in segmentation augment

easymode/segmentation/augmentations.py
@@ -8,13 +8,13 @@ ROT_XZ_YZ_MAX_ANGLE = 25.0
 ROT_XY_MAX_ANGLE = 10.0
 
 def rotate_90_xy(img, label):
-    k = random.randint(0, 3)
+    k = random.randint(0, 4)
     img = np.rot90(img, k=k, axes=(1, 2))
     label = np.rot90(label, k=k, axes=(1, 2))
     return img, label
 
 def rotate_90_xz(img, label):
-    k = random.randint(0, 1) * 2
+    k = random.randint(0, 2) * 2
     img = np.rot90(img, k=k, axes=(0, 2))
     label = np.rot90(label, k=k, axes=(0, 2))
     return img, label

easymode/segmentation/train.py
@@ -8,7 +8,6 @@ AUGMENTATIONS_MISSING_WEDGE = 0.0 # 0.0
 AUGMENTATIONS_GAUSSIAN = 0.5 #0.33 #0.2
 AUGMENTATIONS_SCALE = 0.33 #0.33
 
-
 DEBUG = False
 ROOT = '/cephfs/mlast/compu_projects/easymode'
 if os.name == 'nt':
@@ -99,10 +98,11 @@ class DataLoader:
         if random.uniform(0.0, 1.0) < AUGMENTATIONS_ROT_XY:
             img, label = rotate_continuous_xy(img, label)
 
-        # AUGMENTATION 7 - missing wedge simulation - exclusive
+        # AUGMENTATION 7 - missing wedge simulation
         if random.uniform(0.0, 1.0) < AUGMENTATIONS_MISSING_WEDGE:
             img, label = remove_wedge(img, label)
 
+        # AUGMENTATION 8 - magnification jitter (90% to 110%)
         if random.uniform(0.0, 1.0) < AUGMENTATIONS_SCALE:
             img, label = scale(img, label)
 
@@ -188,21 +188,3 @@ def train_model(title='', features='', batch_size=8, epochs=100, lr_start=1e-3, 
     cb_csv = tf.keras.callbacks.CSVLogger(f'{ROOT}/training/3d/checkpoints/{title}/training_log.csv', append=True)
     model.fit(training_ds, steps_per_epoch=training_steps * 4, validation_data=validation_ds, validation_steps=validation_steps, epochs=epochs, validation_freq=5, callbacks=[cb_checkpoint_val, cb_checkpoint_train, cb_lr, cb_csv])
 
-
-if __name__ == "__main__":
-    features = ['NotMitochondrion3D']
-    loader = DataLoader(features, batch_size=8, validation=False)
-
-    os.makedirs('C:/Users/Mart Last/Desktop/debug', exist_ok=True)
-
-    for k in range(8):
-        datagroup, index = loader.samples[0]
-        img, label = loader.get_sample(datagroup, index)
-        # img, label = loader.augment(img, label)
-        # img, label = loader.preprocess(img, label)
-
-        img_squeeze = np.squeeze(img)
-        label_squeeze = np.squeeze(label)
-
-        mrcfile.write(f'C:/Users/Mart Last/Desktop/debug/test_img_{k}.mrc', img_squeeze, overwrite=True)
-        mrcfile.write(f'C:/Users/Mart Last/Desktop/debug/test_label_{k}.mrc', label_squeeze, overwrite=True)